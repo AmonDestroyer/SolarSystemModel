d02849b217cc6776272d2997bd36a3298716a737
Adam Case <adamrichcase@gmail.com>
2023-03-08 11:49:36 -0800

    Implementation of getPlanetaryData function

--- src/nasaClient/nasaClient.cpp
+++ src/nasaClient/nasaClient.cpp
@@ -1,10 +1,25 @@
 #include "nasaClient/nasaClient.hpp"
+#include <nasaClient/json.hpp>
+
 #include <map>
 #include <iostream>
+#include <stdlib.h>
+#include <regex>
 
+using json = nlohmann::json;
 using std::cout;
 using std::endl;
 using std::cerr;
+
+//===============================================================================================================
+// Helper Function Definition
+//===============================================================================================================
+
+static size_t cb(void *data, size_t size, size_t nmemb, void *clientp);
+struct memory {
+  char *response;
+  size_t size;
+};
 
 std::map<std::string, long>objects = {
     {"Sun", 10},
@@ -19,17 +34,126 @@ std::map<std::string, long>objects = {
     {"Neptune", 899},
     {"JWS", -170}
 };
+
+//===============================================================================================================
+// NasaClient Class
+//...............................................................................................................
+// Constructor and Destructor
+//...............................................................................................................
 NasaClient::NasaClient() {
+    this->curl = curl_easy_init();
+}
+
+NasaClient::~NasaClient() {
+    curl_easy_cleanup(this->curl);
+}
+
+void NasaClient::setEndpoint(std::string endpoint) {
+    this->endpoint = endpoint;
+}
+
+// Public Methods
+void NasaClient::setKey(std::string key) {
+    this->apiKey = key;
+}
+
+void NasaClient::getPlanetaryData(Planet &planet, std::string date) {
+    if (planet.getDataDate() == date)
+        return;
+    struct memory chunk = {0};
+    std::string juliandDate = this->getJulianDate(date);
+    std::string endpoint = "https://ssd.jpl.nasa.gov/api/horizons.api?COMMAND='" + std::to_string(planet.getIndex()) + "'" +
+                            "&EPHEM_TYPE='VECTORS'" +
+                            "&START_TIME='JD" + juliandDate.c_str() + "'" +
+                            "&STOP_TIME='JD" + std::to_string(atol(juliandDate.c_str()) + 1) + "'" +
+                            "&STEP_SIZE='1d'";
+    curl_easy_setopt(this->curl, CURLOPT_URL, endpoint.c_str());
+    curl_easy_setopt(this->curl, CURLOPT_WRITEFUNCTION, cb);
+    curl_easy_setopt(this->curl, CURLOPT_WRITEDATA, (void *)&chunk);
+    CURLcode res = curl_easy_perform(curl);
+    json data = json::parse(chunk.response);
+    free(chunk.response);
+    std::string result = data["result"];
+
+    // Get Position Data
+    size_t index[4];
+    index[0] = result.find("X =");
+    index[1] = result.find("Y =");
+    index[2] = result.find("Z =");
+    index[3] = result.find("VX=");
+    char values[3][BUFSIZ];
+    for (int i=0; i<3; i++) {
+        int offset = 3;
+        int additionalOffset = 1;
+        if (i == 2)
+            additionalOffset ++;
+        int len = index[i+1] - index[i] - (offset + additionalOffset);
+        result.copy(values[i], len, index[i]+offset);
+    }
+    glm::vec3 pos(atof(values[0]), atof(values[1]), atof(values[2]));
+
+    // Get Physical Data
+    //Radius
+    float radius = 0;
+    if (planet.getIndex() > 0) {
+        std::string radiusTag[3] = {"radius", "= ", " "};
+        size_t radiusIndex[3];
+        radiusIndex[0] = result.find(radiusTag[0]);
+        radiusIndex[1] = result.find(radiusTag[1], radiusIndex[0]) + radiusTag[1].length();
+        radiusIndex[2] = result.find(radiusTag[2], radiusIndex[1]);
+        cerr << "DEBUG: " << radiusIndex[0] << ", " << radiusIndex[1] << ", " << radiusIndex[2] << endl;
+        char radiusText[BUFSIZ];
+        result.copy(radiusText, radiusIndex[2] - radiusIndex[1], radiusIndex[1]);
+        radius = atof(radiusText);
+    }
+
+    //Update Data
+    planet.updateData(pos, radius, date);
 }
 
 void NasaClient::test() {
     //Testing of curl
     cerr << "Running Test" << endl;
-    this->curl = curl_easy_init();
-    cerr << "Initiated Curl" << endl;
-    curl_easy_cleanup(this->curl);
+    std::string testDate = "2000-01-01_12:00";
+    std::string julianDate = this->getJulianDate(testDate);
+    cerr << "Converted Julian Date: " << julianDate << endl;
+    std::string calendarDate = this->getCalendarDate(std::to_string(atol(julianDate.c_str()) + 1));
+    cerr << "Converted provided Julian Date to Calendar Date: " << calendarDate << endl;
+    Planet sun("Sun");
+    this->getPlanetaryData(sun, "2023-03-07");
+    Planet jws("JWS");
+    this->getPlanetaryData(jws, "2023-03-07");
     cerr << "Cleaned up Curl" << endl;
 }
+
+//...............................................................................................................
+// Private Methods
+// Judlian Date API: https://ssd-api.jpl.nasa.gov/doc/jd_cal.html
+// Horizon API: https://ssd-api.jpl.nasa.gov/doc/horizons.html
+// Horizon Application: https://ssd.jpl.nasa.gov/horizons/app.html#/
+//...............................................................................................................
+
+std::string NasaClient::getJulianDate(std::string calendarDate) {
+    return convertDate(calendarDate, "cd", "jd");
+}
+
+std::string NasaClient::getCalendarDate(std::string julianDate) {
+    return convertDate(julianDate, "jd", "cd");
+}
+
+std::string NasaClient::convertDate(std::string date, std::string dateType, std::string returnDateType) {
+    struct memory chunk = {0};
+    std::string converterEndpoint = "https://ssd-api.jpl.nasa.gov/jd_cal.api?" + dateType + "=" + date;
+    // curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    curl_easy_setopt(this->curl, CURLOPT_URL, converterEndpoint.c_str());
+    curl_easy_setopt(this->curl, CURLOPT_WRITEFUNCTION, cb);
+    curl_easy_setopt(this->curl, CURLOPT_WRITEDATA, (void *)&chunk);
+    CURLcode res = curl_easy_perform(curl);
+    json data = json::parse(chunk.response);
+    free(chunk.response);
+    return data[returnDateType];
+}
+
 //===============================================================================================================
 // Planet Class
 //...............................................................................................................
@@ -63,3 +187,38 @@ std::string Planet::getName() {
 long Planet::getIndex() {
     return this->index;
 }
+
+//===============================================================================================================
+// Helper Functions
+//===============================================================================================================
+static size_t cb(void *data, size_t size, size_t nmemb, void *clientp)
+{
+  size_t realsize = size * nmemb;
+  struct memory *mem = (struct memory *)clientp;
+ 
+  char *ptr = (char *)realloc(mem->response, mem->size + realsize + 1);
+  if(ptr == NULL)
+    return 0;  /* out of memory! */
+ 
+  mem->response = ptr;
+  memcpy(&(mem->response[mem->size]), data, realsize);
+  mem->size += realsize;
+  mem->response[mem->size] = 0;
+ 
+  return realsize;
+}
+
+
+
+// Below is code use to check for curl run errors and to print them if they arrise
+// char errbuf[CURL_ERROR_SIZE];
+    // if (res != CURLE_OK)
+    // {
+    //     size_t len = std::strlen(errbuf);
+    //     fprintf(stderr, "\nlibcurl: (%d) ", res);
+    //     if(len)
+    //         fprintf(stderr, "%s%s", errbuf,
+    //                 ((errbuf[len - 1] != '\n') ? "\n" : ""));
+    //     else
+    //         fprintf(stderr, "%s\n", curl_easy_strerror(res));
+    // }
--- src/nasaClient/nasaClient.hpp
+++ src/nasaClient/nasaClient.hpp
@@ -4,6 +4,9 @@
 #define CURL_STATICLIB
 #include <curl/curl.h>
 #include <string>
+#include <vector>
+#include <glm/vec3.hpp>
+
 class Planet {
 private:
     std::string name;
@@ -31,12 +34,31 @@ private:
      * 
      */
     CURL* curl;
+    std::string endpoint;
+    std::string apiKey;
+
+    std::string getJulianDate(std::string calendarDate);
+    std::string getCalendarDate(std::string julianDate);
+    std::string convertDate(std::string date, std::string dateType, std::string returnDateType);
+
 public:
     /**
      * @brief Construct a new Client object
      * 
      */
     NasaClient();
+
+    /**
+     * @brief Destroy the Nasa Client object
+     * 
+     */
+    ~NasaClient();
+
+    void setEndpoint(std::string endpoint);
+
+    void setKey(std::string key);
+
+    void getPlanetaryData(Planet &planet, std::string date);
 
     /**
      * @brief Used to test if the client object works correctly with curl
